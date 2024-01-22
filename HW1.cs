1f06133d6def8884f71e2c9f4bb0551d8a6e7dc9
JaTorres9 <156493707+JaTorres9@users.noreply.github.com>
2024-01-21 21:32:53 -0600

    HomeWork1 code file

---
+++ HW1.cs
@@ -0,0 +1,14 @@
+﻿namespace HomeWork1;
+
+class Program
+{
+    static void Main(string[] args)
+    { //Program to solve Linear Line Equation Z=4X^2+3Y
+        double X = 2.5;
+        double Y = 3.3;
+        double Z = (4*X*X) + 3*Y;
+        Console.WriteLine("X = 2.5 , Y = 3.3");
+        Console.WriteLine("The value of Z is: "+Z);
+    }
+}
+
